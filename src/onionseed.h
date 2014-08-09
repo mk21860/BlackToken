b525102dffd191f67a7cc88431c8c35dcd966ec9
root <root@9731748.(none)>
2014-08-08 21:51:46 -0400

    first commit

---
+++ src/onionseed.h
@@ -0,0 +1,18 @@
+
+#ifndef STEALTH_ONIONSEED_H
+#define STEALTH_ONIONSEED_H
+
+// hidden service seeds
+static const char *strMainNetOnionSeed[][1] = {
+    {"4havbfjumkembour.onion"},
+    {NULL},
+    {NULL}
+};
+
+static const char *strTestNetOnionSeed[][1] = {
+    {"4havbfjumkembour.onion"},
+    {NULL},
+    {NULL}
+};
+
+#endif
