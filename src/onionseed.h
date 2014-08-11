44326446368f2d4f656f9c88585570db37ae2489
mk21860 <mj21860@gmail.com>
2014-08-11 09:19:40 -0700

    Update onionseed.h

--- src/onionseed.h
+++ src/onionseed.h
@@ -6,13 +6,15 @@
 static const char *strMainNetOnionSeed[][1] = {
     {"6qhc2bnrrhvboss6.onion"},
     {"p4jo5errcewsvyz2.onion"},
-    {"l6qc2pyxj4trlvdg.onion"}
+    {"l6qc2pyxj4trlvdg.onion"},
+    {NULL}
 };
 
 static const char *strTestNetOnionSeed[][1] = {
     {"6qhc2bnrrhvboss6.onion"},
     {"p4jo5errcewsvyz2.onion"},
-    {"l6qc2pyxj4trlvdg.onion"}
+    {"l6qc2pyxj4trlvdg.onion"},
+    {NULL}
 };
 
 #endif
