dea67d33d31a2d6c9ea380bb6e71d0273fb14561
mk21860 <mj21860@gmail.com>
2014-08-11 07:35:12 -0700

    added more seed nodes

--- src/onionseed.h
+++ src/onionseed.h
@@ -4,15 +4,15 @@
 
 // hidden service seeds
 static const char *strMainNetOnionSeed[][1] = {
-    {"4havbfjumkembour.onion"},
-    {NULL},
-    {NULL}
+    {"6qhc2bnrrhvboss6.onion"},
+    {"p4jo5errcewsvyz2.onion"},
+    {"l6qc2pyxj4trlvdg.onion"}
 };
 
 static const char *strTestNetOnionSeed[][1] = {
-    {"4havbfjumkembour.onion"},
-    {NULL},
-    {NULL}
+    {"6qhc2bnrrhvboss6.onion"},
+    {"p4jo5errcewsvyz2.onion"},
+    {"l6qc2pyxj4trlvdg.onion"}
 };
 
 #endif
